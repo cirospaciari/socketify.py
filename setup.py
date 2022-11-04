bf5325d0f23bd0d0b2b32e54a12c0f956c8d259f
Ciro <ciro.spaciari@gmail.com>
2022-11-04 19:54:38 -0300

    new workflow, new setup.py

--- setup.py
+++ setup.py
@@ -4,53 +4,53 @@ vi = sys.version_info
 if vi < (3, 7):
     raise RuntimeError('socketify requires Python 3.7 or greater')
 
-if sys.platform in ('win32', 'cygwin', 'cli'):
-    raise RuntimeError('socketify does not support Windows at the moment')
+# if sys.platform in ('win32', 'cygwin', 'cli'):
+#     raise RuntimeError('socketify does not support Windows at the moment')
 
 import setuptools
-from setuptools.command.sdist import sdist
-from setuptools.command.build_ext import build_ext
+# from setuptools.command.sdist import sdist
+# from setuptools.command.build_ext import build_ext
 
-import pathlib
-import os
-import shutil
-import subprocess
+# import pathlib
+# import os
+# import shutil
+# import subprocess
 
-_ROOT = pathlib.Path(__file__).parent
+# _ROOT = pathlib.Path(__file__).parent
 
-UWS_DIR = str(_ROOT / "src" / "socketify" /"uWebSockets")
-UWS_BUILD_DIR = str(_ROOT / "build" /"uWebSockets")
+# UWS_DIR = str(_ROOT / "src" / "socketify" /"uWebSockets")
+# UWS_BUILD_DIR = str(_ROOT / "build" /"uWebSockets")
 
-NATIVE_CAPI_DIR = str(_ROOT / "build" / "native") 
-NATIVE_LIB_PATH = str(_ROOT / "build" / "libsocketify.so")
-NATIVE_DIR = str(_ROOT / "src" / "socketify" /"native")
-NATIVE_BUILD_DIR = str(_ROOT / "build" /"native")
-NATIVE_LIB_OUTPUT = str(_ROOT / "src" / "socketify" / "libsocketify.so")
-
-
+# NATIVE_CAPI_DIR = str(_ROOT / "build" / "native") 
+# NATIVE_LIB_PATH = str(_ROOT / "build" / "libsocketify.so")
+# NATIVE_DIR = str(_ROOT / "src" / "socketify" /"native")
+# NATIVE_BUILD_DIR = str(_ROOT / "build" /"native")
+# NATIVE_LIB_OUTPUT = str(_ROOT / "src" / "socketify" / "libsocketify.so")
 
 
-class Prepare(sdist):
-    def run(self):
-        super().run()
 
 
-class Makefile(build_ext):
-    def run(self):
-        env = os.environ.copy()
+# class Prepare(sdist):
+#     def run(self):
+#         super().run()
+
+
+# class Makefile(build_ext):
+#     def run(self):
+#         env = os.environ.copy()
         
-        if os.path.exists(UWS_BUILD_DIR):
-            shutil.rmtree(UWS_BUILD_DIR)
-        shutil.copytree(UWS_DIR, UWS_BUILD_DIR)
+#         if os.path.exists(UWS_BUILD_DIR):
+#             shutil.rmtree(UWS_BUILD_DIR)
+#         shutil.copytree(UWS_DIR, UWS_BUILD_DIR)
 
-        if os.path.exists(NATIVE_CAPI_DIR):
-            shutil.rmtree(NATIVE_CAPI_DIR)
-        shutil.copytree(NATIVE_DIR, NATIVE_CAPI_DIR)
+#         if os.path.exists(NATIVE_CAPI_DIR):
+#             shutil.rmtree(NATIVE_CAPI_DIR)
+#         shutil.copytree(NATIVE_DIR, NATIVE_CAPI_DIR)
 
-        subprocess.run(["make", "shared"], cwd=NATIVE_CAPI_DIR, env=env, check=True)
-        shutil.move(NATIVE_LIB_PATH, NATIVE_LIB_OUTPUT)
+#         subprocess.run(["make", "shared"], cwd=NATIVE_CAPI_DIR, env=env, check=True)
+#         shutil.move(NATIVE_LIB_PATH, NATIVE_LIB_OUTPUT)
         
-        super().run()
+#         super().run()
 
 
 with open("README.md", "r", encoding="utf-8") as fh:
@@ -81,7 +81,7 @@ setuptools.setup(
     python_requires=">=3.7",
     install_requires=["cffi>=1.0.0", "setuptools>=60.0.0"],
     has_ext_modules=lambda: True,
-    cmdclass={'sdist': Prepare}, #cmdclass={'sdist': Prepare, 'build_ext': Makefile},
+    cmdclass={}, #cmdclass={'sdist': Prepare, 'build_ext': Makefile},
 
     include_package_data=True
 )
