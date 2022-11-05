6035891526675f87a04d4cc54c49cc31cadf4be1
Ciro <ciro.spaciari@gmail.com>
2022-11-05 08:37:19 -0300

    added DLL EXPORT

--- src/socketify/native/src/libsocketify.h
+++ src/socketify/native/src/libsocketify.h
@@ -8,23 +8,23 @@
 extern "C"
 {
 #endif
-typedef void (*socketify_prepare_handler)(void* user_data);
-typedef void (*socketify_timer_handler)(void* user_data);
+DLL_EXPORT typedef void (*socketify_prepare_handler)(void* user_data);
+DLL_EXPORT typedef void (*socketify_timer_handler)(void* user_data);
 
-typedef enum {
+DLL_EXPORT typedef enum {
   SOCKETIFY_RUN_DEFAULT = 0,
   SOCKETIFY_RUN_ONCE,
   SOCKETIFY_RUN_NOWAIT
 } socketify_run_mode;
 
-typedef struct {
+DLL_EXPORT typedef struct {
     void* uv_prepare_ptr;
     socketify_prepare_handler on_prepare_handler;
     void* on_prepare_data;
     void* uv_loop;
 } socketify_loop;
 
-typedef struct{
+DLL_EXPORT typedef struct{
     void* uv_timer_ptr;
     socketify_timer_handler handler;
     void* user_data;
@@ -32,22 +32,22 @@ typedef struct{
 
 
 
-socketify_loop * socketify_create_loop();
-bool socketify_constructor_failed(socketify_loop* loop);
-bool socketify_on_prepare(socketify_loop* loop, socketify_prepare_handler handler, void* user_data);
-bool socketify_prepare_unbind(socketify_loop* loop);
-void socketify_destroy_loop(socketify_loop* loop);
-void* socketify_get_native_loop(socketify_loop* loop);
+DLL_EXPORT socketify_loop * socketify_create_loop();
+DLL_EXPORT bool socketify_constructor_failed(socketify_loop* loop);
+DLL_EXPORT bool socketify_on_prepare(socketify_loop* loop, socketify_prepare_handler handler, void* user_data);
+DLL_EXPORT bool socketify_prepare_unbind(socketify_loop* loop);
+DLL_EXPORT void socketify_destroy_loop(socketify_loop* loop);
+DLL_EXPORT void* socketify_get_native_loop(socketify_loop* loop);
 
-int socketify_loop_run(socketify_loop* loop, socketify_run_mode mode);
-void socketify_loop_stop(socketify_loop* loop);
+DLL_EXPORT int socketify_loop_run(socketify_loop* loop, socketify_run_mode mode);
+DLL_EXPORT void socketify_loop_stop(socketify_loop* loop);
 
-socketify_timer* socketify_create_timer(socketify_loop* loop, uint64_t timeout, uint64_t repeat, socketify_timer_handler handler, void* user_data);
-void socketify_timer_destroy(socketify_timer* timer);
-void socketify_timer_set_repeat(socketify_timer* timer, uint64_t repeat);
+DLL_EXPORT socketify_timer* socketify_create_timer(socketify_loop* loop, uint64_t timeout, uint64_t repeat, socketify_timer_handler handler, void* user_data);
+DLL_EXPORT void socketify_timer_destroy(socketify_timer* timer);
+DLL_EXPORT void socketify_timer_set_repeat(socketify_timer* timer, uint64_t repeat);
 
-socketify_timer* socketify_create_check(socketify_loop* loop, socketify_timer_handler handler, void* user_data);
-void socketify_check_destroy(socketify_timer* timer);
+DLL_EXPORT socketify_timer* socketify_create_check(socketify_loop* loop, socketify_timer_handler handler, void* user_data);
+DLL_EXPORT void socketify_check_destroy(socketify_timer* timer);
 #endif
 #ifdef __cplusplus
 }
