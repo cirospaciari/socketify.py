8999d823c1a3b0c81d4c31aae754db4b48524cb7
Ciro <ciro.spaciari@gmail.com>
2022-11-07 14:49:35 -0300

    add sync upgrade

---
+++ examples/upgrade.py
@@ -0,0 +1,32 @@
+from socketify import App, AppOptions, OpCode, CompressOptions
+import asyncio
+
+def ws_open(ws):
+    print('A WebSocket got connected!')
+    ws.send("Hello World!", OpCode.TEXT)
+
+def ws_message(ws, message, opcode):
+    print(message, opcode)
+    #Ok is false if backpressure was built up, wait for drain
+    ok = ws.send(message, opcode)
+
+def ws_upgrade(res, req, socket_context):
+    key = req.get_header("sec-websocket-key")
+    protocol = req.get_header("sec-websocket-protocol")
+    extensions = req.get_header("sec-websocket-extensions")
+    res.upgrade(key, protocol, extensions, socket_context)
+    
+app = App()    
+app.ws("/*", {
+    'compression': CompressOptions.SHARED_COMPRESSOR,
+    'max_payload_length': 16 * 1024 * 1024,
+    'idle_timeout': 12,
+    'open': ws_open,
+    'message': ws_message,
+    'upgrade': ws_upgrade,
+    'drain': lambda ws: print('WebSocket backpressure: %s', ws.get_buffered_amount()),
+    'close': lambda ws, code, message: print('WebSocket closed')
+})
+app.any("/", lambda res,req: res.end("Nothing to see here!'"))
+app.listen(3000, lambda config: print("Listening on port http://localhost:%d now\n" % (config.port)))
+app.run()
