c4109326d078e5d81ecf20f779bc9080ac881872
Ciro <ciro.spaciari@gmail.com>
2022-11-20 08:01:13 -0300

    add dll copying fixing #37

--- setup.py
+++ setup.py
@@ -79,6 +79,7 @@ setuptools.setup(
     package_data={
         "": [
             "./*.so",
+            "./*.dll",
             "./uWebSockets/*",
             "./uWebSockets/*/*",
             "./uWebSockets/*/*/*",
