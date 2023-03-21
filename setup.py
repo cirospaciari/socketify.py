40404519e54b36adec3dca9a6a5fdd028aaf5bd4
Ciro <ciro.spaciari@gmail.com>
2023-03-21 13:33:30 -0300

    bump version

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="socketify",
-    version="0.0.19",
+    version="0.0.20",
     platforms=["any"],
     author="Ciro Spaciari",
     author_email="ciro.spaciari@gmail.com",
